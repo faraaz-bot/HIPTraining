6d859d5bfaae4bd12e195792c97eb34a98938df0
Malcolm Roberts <malcolm.roberts@amd.com>
2023-09-29 16:20:55 -0600

    Work on latency exercises.

--- latency_and_occupancy/ex2_occupancy.cpp
+++ latency_and_occupancy/ex2_occupancy.cpp
@@ -1,6 +1,18 @@
 #include<iostream>
+#include<vector>
 
 #include <hip/hip_runtime.h>
+
+
+// Computes ceil(numerator/divisor) for integer types.
+template <typename intT1,
+          class = typename std::enable_if<std::is_integral<intT1>::value>::type,
+          typename intT2,
+          class = typename std::enable_if<std::is_integral<intT2>::value>::type>
+intT1 ceildiv(const intT1 numerator, const intT2 divisor)
+{
+    return (numerator + divisor - 1) / divisor;
+}
 
 __global__ void vecAdd(float* a, const float* b, const int N)
 {
@@ -14,19 +26,71 @@ __global__ void vecAdd(float* a, const float* b, const int N)
 int main()
 {
 
+    const int N = 1<<15;
+
+    
+    
     {
-        size_t lds_bytes = 1 << 12;
-        dim3 blockDim(N, 1, 1);
+        size_t lds_bytes = 0;//1 << 2;
+
+        int blockSize = 512;
+        int gridSize = ceildiv(N, blockSize);
+        
+        dim3 gridDim(gridSize);
+        dim3 blockDim(blockSize);
         int max_blocks_per_sm = 0;
         
-        const auto ret = hipOccupancyMaxActiveBlocksPerMultiprocessor(
-            &max_blocks_per_sm, vecAdd, blockDim.x * blockDim.y * blockDim.z, lds_bytes);
+        auto ret = hipOccupancyMaxPotentialBlockSize(
+            &gridSize, // int* gridSize
+            &blockSize, // int* blockSize,
+            vecAdd, //hipFunction_t f,
+            lds_bytes, //size_t dynSharedMemPerBlk,
+            1024 //int blockSizeLimit 
+            );
+                            
+        // const auto ret = hipOccupancyMaxActiveBlocksPerMultiprocessor(
+        //     &max_blocks_per_sm, vecAdd, blockDim.x * blockDim.y * blockDim.z, lds_bytes);
         if(ret != hipSuccess)  {
-            throw std::runtime_error("kernel execution failed");
+            std::stringstream ss;
+            ss << "hipOccupancyMaxActiveBlocksPerMultiprocessorkernel failed with code ";
+            ss << ret;
+            ss << " " << hipGetErrorName(ret);
+            ss << " which means: " << hipGetErrorString(ret);
+            throw std::runtime_error(ss.str());
         }
+        std::cout << "gridSize: " << gridSize << "\tblockSize: " << blockSize << "\n";
+
         std::cout << "occupancy: " << max_blocks_per_sm << "\n";
+
+        std::vector<float> vala(N);
+        std::vector<float> valb(N);
+
+        const size_t valbytes = vala.size() * sizeof(decltype(vala)::value_type);
+        float* d_a = nullptr;
+        float* d_b = nullptr;
+        ret = hipMalloc(&d_a, valbytes);
+        if(ret != hipSuccess)  {
+            throw std::runtime_error("hipMalloc faile");
+        }
+        ret = hipMalloc(&d_b, valbytes);
+        if(ret != hipSuccess)  {
+            throw std::runtime_error("hipMalloc faile");
+        }
+        if(hipMemcpy(d_a, vala.data(), valbytes, hipMemcpyHostToDevice) != hipSuccess)
+        {
+            throw std::runtime_error("hipMemcpy failed");
+        }
+        if(hipMemcpy(d_b, valb.data(), valbytes, hipMemcpyHostToDevice) != hipSuccess)
+        {
+            throw std::runtime_error("hipMemcpy failed");
+        }
+
+
+        
+        vecAdd<<<gridDim, blockDim, lds_bytes >>>(d_a, d_b, N);
     }
 
+    
     // TODO: add timing and launch stuff.
     // TODO: pass stuff via command-line?
     
