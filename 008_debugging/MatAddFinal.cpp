674dbf1a729e24b8e00e58b1e22fcec8ecc413f5
Corey Derochie <corey.derochie@amd.com>
2024-06-03 10:57:47 -0500

    Added session #8 debugging code

---
+++ 008_debugging/MatAddFinal.cpp
@@ -0,0 +1,82 @@
+#include <hip/hip_runtime.h>
+#include <iostream>
+#include <vector>
+
+__global__
+void deviceMatrixAdd(float* c, const float* a, const float* b, int width, int height)
+{
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+    int idx = y * width + x;
+    if (x < width && y < height)
+        c[idx] = a[idx] + b[idx];
+}
+
+int ceilDiv(int num, int div)
+{
+    return (num + div - 1) / div;
+}
+
+void hostVectorAdd(std::vector<float>& c, const std::vector<float>& a, const std::vector<float>& b)
+{
+    for (int i = 0; i < c.size(); ++i)
+        c[i] = a[i] + b[i];
+}
+
+bool compareVectors(const std::vector<float>& ref, const std::vector<float>& test)
+{
+    for (int i = 0; i < ref.size(); ++i)
+    {
+        if (ref[i] != test[i])
+        {
+            std::cout << "Mismatch at index " << i << ": " << ref[i] << " != " << test[i] << std::endl;
+            return false;
+        }
+    }
+    return true;
+}
+
+void fillArray(std::vector<float>& v)
+{
+    for (int i = 0; i < v.size(); ++i)
+        v[i] = i;
+}
+
+int main()
+{
+    std::cout << "HIP matrix addition example" << std::endl;
+
+    const int width = 1000000;
+    const int height = 100;
+    const int SIZE = width * height;
+
+    std::vector<float> hostA(SIZE);
+    fillArray(hostA);
+    std::vector<float> hostB(SIZE);
+    fillArray(hostB);
+
+    float* devA = nullptr;
+    assert(hipMalloc(&devA, SIZE * sizeof(float)) == hipSuccess);
+    assert(hipMemcpy(devA, hostA.data(), SIZE * sizeof(float), hipMemcpyHostToDevice) == hipSuccess);
+
+    float* devB = nullptr;
+    assert(hipMalloc(&devB, SIZE * sizeof(float)) == hipSuccess);
+    assert(hipMemcpy(devB, hostB.data(), SIZE * sizeof(float), hipMemcpyHostToDevice) == hipSuccess);
+
+    float* devC = nullptr;
+    assert(hipMalloc(&devC, SIZE * sizeof(float)) == hipSuccess);
+
+    dim3 blockSize(8, 8);
+    dim3 blocks(ceilDiv(width, blockSize.x), ceilDiv(height, blockSize.y));
+    deviceMatrixAdd<<<blocks, blockSize>>>(devC, devA, devB, width, height);
+
+    std::vector<float> refC(SIZE);
+    hostVectorAdd(refC, hostA, hostB);
+    std::vector<float> testC(SIZE);
+    assert(hipMemcpy(testC.data(), devC, SIZE * sizeof(float), hipMemcpyDeviceToHost) == hipSuccess);
+
+    if (compareVectors(refC, testC))
+        std::cout << "Test passed" << std::endl;
+
+    return 0;
+}
---
+++ 008_debugging/MatAdd_3_ArraySize.cpp
@@ -0,0 +1,82 @@
+#include <hip/hip_runtime.h>
+#include <iostream>
+#include <vector>
+
+__global__
+void deviceMatrixAdd(float* c, const float* a, const float* b, int width, int height)
+{
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+    int idx = y * width + x;
+    c[idx] = a[idx] + b[idx];
+}
+
+void vectorAdd(std::vector<float>& c, const std::vector<float>& a, const std::vector<float>& b)
+{
+    for (int i = 0; i < c.size(); ++i)
+        c[i] = a[i] + b[i];
+}
+
+bool compareVectors(const std::vector<float>& ref, const std::vector<float>& test)
+{
+    for (int i = 0; i < ref.size(); ++i)
+    {
+        if (ref[i] != test[i])
+        {
+            std::cout << "Mismatch at index " << i << ": " << ref[i] << " != " << test[i] << std::endl;
+            return false;
+        }
+    }
+    return true;
+}
+
+void fillArray(std::vector<float>& v)
+{
+    for (int i = 0; i < v.size(); ++i)
+        v[i] = i;
+}
+
+int ceilDiv(int num, int div)
+{
+    return (num + div - 1) / div;
+}
+
+int main()
+{
+    std::cout << "HIP matrix addition example" << std::endl;
+
+    const int width = 64;
+    const int height = 64;
+    const int SIZE = width * height;
+
+    std::vector<float> hostA(SIZE);
+    fillArray(hostA);
+    std::vector<float> hostB(SIZE);
+    fillArray(hostB);
+
+    float* devA = nullptr;
+    assert(hipMalloc(&devA, SIZE) == hipSuccess);
+    assert(hipMemcpy(devA, hostA.data(), SIZE, hipMemcpyHostToDevice) == hipSuccess);
+
+    float* devB = nullptr;
+    assert(hipMalloc(&devB, SIZE) == hipSuccess);
+    assert(hipMemcpy(devB, hostB.data(), SIZE, hipMemcpyHostToDevice) == hipSuccess);
+
+    float* devC = nullptr;
+    assert(hipMalloc(&devC, SIZE) == hipSuccess);
+
+    dim3 blockSize(8, 8);
+    dim3 blocks(ceilDiv(width, blockSize.x), ceilDiv(height, blockSize.y));
+    deviceMatrixAdd<<<blocks, blockSize>>>(devC, devA, devB, width, height);
+    hipDeviceSynchronize();
+
+    std::vector<float> refC(SIZE);
+    vectorAdd(refC, hostA, hostB);
+    std::vector<float> testC(SIZE);
+    assert(hipMemcpy(testC.data(), devC, SIZE, hipMemcpyDeviceToHost) == hipSuccess);
+
+    if (compareVectors(refC, testC))
+        std::cout << "Test passed" << std::endl;
+
+    return 0;
+}
